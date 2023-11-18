e0d7a55d95fa918c1fdfc4109a9388edcc62c9da
Vsevolod Babansky <117068525+SBrep@users.noreply.github.com>
2023-11-18 08:52:35 +0000

    Задания 1-3

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,27 @@
 
 void RevertString(char *str)
 {
-	// your code here
-}
+	char* start_of_str = str;	//переменная хранит адрес первогот элемента строки
 
+	int length_of_str = 0;		//длина строки
+	if (str == 0){				//если указатель нулевой, то возврат из функции
+		return;
+	}
+
+	//цикл для поиска длины строки
+	while (*str != '\0'){
+		str++;
+		length_of_str++;
+	}
+
+	//возврат указателя к началу данных нашей строки
+	str = start_of_str;
+
+	//цикл для реверса строки
+	for (int i = 0, j = length_of_str - 1; i < j; i++, j--)
+    {
+        char ch = str[i];
+        str[i] = str[j];
+        str[j] = ch;
+    }
+}
